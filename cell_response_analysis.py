55e08da367a1ec31cccb78e072746db13f78bbd8
arpi5191 <69826982+arpi5191@users.noreply.github.com>
2025-10-26 18:11:48 -0700

    Adding updated part 3 to Git

--- cell_response_analysis.py
+++ cell_response_analysis.py
@@ -1,27 +1,40 @@
-import sqlite3
+from db import CellDataLoader
+import pandas as pd
 
-conn = sqlite3.connect("cell_data.db")
-cursor = conn.cursor()
+class CellResponseAnalysis:
+    def __init__(self, db_path="cell_data.db"):
+        self.loader = CellDataLoader(db_path)
 
-query = """
-    SELECT
-        su.subject,
-        sa.response,
-        c.population,
-        SUM(c.count) AS total_population_count,
-        SUM(c.total_count) AS total_count,
-        ROUND(100.0 * SUM(c.count) / SUM(c.total_count), 2) AS percentage
-    FROM subjects AS su
-    JOIN samples AS sa ON sa.subject = su.subject
-    JOIN cell_summary AS c ON c.sample = sa.sample
-    WHERE sa.condition = "melanoma" AND sa.treatment = "miraclib" AND sa.sample_type = "PBMC"
-          AND (sa.response = "yes" OR sa.response = "no")
-    GROUP BY su.subject, c.population, sa.response
-    ORDER BY su.subject, c.population;
-"""
-cursor.execute(query)
+    def compute_response(self):
+        query = """
+                    SELECT
+                        su.subject,
+                        sa.response,
+                        c.population,
+                        SUM(c.count) AS total_population_count,
+                        SUM(c.total_count) AS total_count,
+                        ROUND(100.0 * SUM(c.count) / SUM(c.total_count), 2) AS percentage
+                    FROM subjects AS su
+                    JOIN samples AS sa ON sa.subject = su.subject
+                    JOIN cell_summary AS c ON c.sample = sa.sample
+                    WHERE sa.condition = "melanoma" AND sa.treatment = "miraclib" AND sa.sample_type = "PBMC"
+                          AND (sa.response = "yes" OR sa.response = "no")
+                    GROUP BY su.subject, c.population, sa.response
+                    ORDER BY su.subject, c.population;
+                """
 
-rows = cursor.fetchall()
+        self.loader.cursor.execute(query)
+        rows = self.loader.cursor.fetchall()
 
-for row in rows:
-    print(row)
+        cell_response_df = pd.DataFrame(rows, columns=['subject', 'response', 'population',
+                                                       'total_population_count', 'total_count', 'percentage'])
+
+        csv_filename = "cell_response.csv"
+        cell_response_df.to_csv(csv_filename, index=False)
+
+def main():
+    responder = CellResponseAnalysis()
+    responder.compute_response()
+
+if __name__ == "__main__":
+    main()
