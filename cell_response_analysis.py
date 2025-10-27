5d40199622c04e588b99d32ce575f0d42352c321
arpi5191 <69826982+arpi5191@users.noreply.github.com>
2025-10-26 17:41:18 -0700

    Adding updated cell_response query with filtering yes/no but assuming the constraints are the same

--- cell_response_analysis.py
+++ cell_response_analysis.py
@@ -7,12 +7,15 @@ query = """
     SELECT
         su.subject,
         c.population,
+        sa.response,
         SUM(c.count) AS total_population_count,
         SUM(c.total_count) AS total_count,
         ROUND(100.0 * SUM(c.count) / SUM(c.total_count), 2) AS percentage
     FROM subjects AS su
     JOIN samples AS sa ON sa.subject = su.subject
     JOIN cell_summary AS c ON c.sample = sa.sample
+    WHERE sa.condition = "melanoma" AND sa.treatment = "miraclib" AND sa.sample_type = "PBMC"
+          AND (sa.response = "yes" or sa.response = "no")
     GROUP BY su.subject, c.population
     ORDER BY su.subject, c.population;
 """
